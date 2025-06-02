4169f283a46c44d9282a796df531041d8b87b55c
danieldivincenzo <danieldivincenzo14@gmail.com>
2025-06-02 13:04:42 +0200

    recuperati elementi dal DOM

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// Recupero tutti gli elementi di input dati dal DOM
+const inputNome = document.getElementById("inputNome")
+const inputCognome = document.getElementById("inputCognome")
+const inputEmail = document.getElementById("inputEmail")
+const selectLavoro = document.getElementById("selectLavoro")
+const inputTextarea = document.getElementById("textarea")
+const inputPromo = document.getElementById("inputPromo")
+console.log(inputNome, inputCognome, inputEmail, selectLavoro, inputTextarea, inputPromo)
+
+// Recupero il bottone dal DOM
+const btnPrev = document.getElementById("btnPrev")
+console.log(btnPrev)
+
+// Recupero il div dove andrà il prezzo formattato del preventivo
+const divRisultato = document.getElementById("divRisultato")
+console.log(divRisultato)
+
+
+const oreLavoro = 10  // Ore di lavoro di default come da consegna
+
+// Ascolto il click del mouse sul button
+btnPrev.addEventListener("click", function () {
+
+})
